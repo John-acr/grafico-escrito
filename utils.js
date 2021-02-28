aa5a246ca3b94fbcd2251ab78c3939fb4a02dc4d
John-acr <79087680+John-acr@users.noreply.github.com>
2021-02-28 13:04:51 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
+
 export async function getData() {
-  //const response = await fetch('https://https://api.covid19api.com')
+  // const response = await fetch('https://api.covid19api.com/all')
   const response = await fetch('https://api.covid19api.com/live/country/peru')
   const data = await response.json()
   return data
@@ -8,15 +9,13 @@ export async function getData() {
 export function sortByDate(data) {
   return import('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.15/lodash.min.js')
   .then(() => {
-    const recovered = _.sortBy(Object.values(data.Recovered), ['date'])
-    const deaths = _.sortBy(Object.values(data.Deaths), ['date'])
-    const confirmed = _.sortBy(Object.values(data.Confirmed), ['date'])
-    const active = _.sortBy(Object.values(data.Active), ['date'])
+    const recovered = _.sortBy(Object.values(data.recovered), ['date'])
+    const deaths = _.sortBy(Object.values(data.deaths), ['date'])
+    const confirmed = _.sortBy(Object.values(data.confirmed), ['date'])
     return {
       recovered,
       deaths,
       confirmed,
-      active,
     }
   })
 }
@@ -27,7 +26,6 @@ export async function getTotalCasesByDate() {
     confirmed: {},
     recovered: {},
     deaths: {},
-    active: {},
   }
   data.forEach((item) => {
     try {
